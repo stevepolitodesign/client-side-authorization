40302fc81ed60ba76ae69f2af4a2a1fd8e4271c5
Steve Polito <info@stevepolitodesign.com>
2019-04-06 16:16:33 -0400

    write privatePage class

--- js/privatePage.js
+++ js/privatePage.js
@@ -2,6 +2,19 @@ class privatePage {
     constructor(redirectUrl = '/', message = 'You are not authorized to view this page') {
         this.redirectUrl = redirectUrl;
         this.message = message;
-        this.path = window.location.pathname;
+        this.url = window.location.href;
+        this.handlePageLoad();
+    }
+
+    handlePageLoad() {
+        let urls = this.getWhiteListURLs();
+        if ( urls.indexOf(this.url) === -1 ) {
+            alert(this.message);
+            window.location =  this.redirectUrl;
+        }
+    }
+
+    getWhiteListURLs() {
+        return localStorage.getItem('whiteListedUrls') ? JSON.parse(localStorage.getItem('whiteListedUrls')) : [];
     }
 }
